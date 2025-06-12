7ae682c5cd2116b6e35dfd4ee87ed65c52c98ced
michaelxestep <michaelestepu@gmail.com>
2025-06-12 09:30:05 -0400

    feat: incremental

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ function initMap() {
     center: { lat: -33.8688, lng: 151.2195 },
     zoom: 13,
   });
-  const input = document.getElementById("pac-input");
-  // Specify just the place data fields that you need.
-  const autocomplete = new google.maps.places.Autocomplete(input, {
-    fields: ["place_id", "geometry", "formatted_address", "name"],
-  });
 
-  autocomplete.bindTo("bounds", map);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  const marker = new google.maps.AdvancedMarkerElement({
+    map: map,
+    position: { lat: -25.344, lng: 131.031 },
+    title: "Uluru",
+  });
+  // const input = document.getElementById("pac-input");
+  // // Specify just the place data fields that you need.
+  // const autocomplete = new google.maps.places.Autocomplete(input, {
+  //   fields: ["place_id", "geometry", "formatted_address", "name"],
+  // });
+
+  // autocomplete.bindTo("bounds", map);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   const infowindow = new google.maps.InfoWindow();
   const infowindowContent = document.getElementById("infowindow-content");
@@ -29,36 +35,36 @@ function initMap() {
   marker.addListener("click", () => {
     infowindow.open(map, marker);
   });
-  autocomplete.addListener("place_changed", () => {
-    infowindow.close();
+  // autocomplete.addListener("place_changed", () => {
+  //   infowindow.close();
 
-    const place = autocomplete.getPlace();
+  //   const place = autocomplete.getPlace();
 
-    if (!place.geometry || !place.geometry.location) {
-      return;
-    }
+  //   if (!place.geometry || !place.geometry.location) {
+  //     return;
+  //   }
 
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
+  //   if (place.geometry.viewport) {
+  //     map.fitBounds(place.geometry.viewport);
+  //   } else {
+  //     map.setCenter(place.geometry.location);
+  //     map.setZoom(17);
+  //   }
 
-    // Set the position of the marker using the place ID and location.
-    // @ts-ignore This should be in @typings/googlemaps.
-    marker.setPlace({
-      placeId: place.place_id,
-      location: place.geometry.location,
-    });
-    marker.setVisible(true);
-    infowindowContent.children.namedItem("place-name").textContent = place.name;
-    infowindowContent.children.namedItem("place-id").textContent =
-      place.place_id;
-    infowindowContent.children.namedItem("place-address").textContent =
-      place.formatted_address;
-    infowindow.open(map, marker);
-  });
+  //   // Set the position of the marker using the place ID and location.
+  //   // @ts-ignore This should be in @typings/googlemaps.
+  //   marker.setPlace({
+  //     placeId: place.place_id,
+  //     location: place.geometry.location,
+  //   });
+  //   marker.setVisible(true);
+  //   infowindowContent.children.namedItem("place-name").textContent = place.name;
+  //   infowindowContent.children.namedItem("place-id").textContent =
+  //     place.place_id;
+  //   infowindowContent.children.namedItem("place-address").textContent =
+  //     place.formatted_address;
+  //   infowindow.open(map, marker);
+  // });
 }
 
 window.initMap = initMap;
