7e7dbca30649fdedb12907c73873a31d2265767f
michaelxestep <michaelestepu@gmail.com>
2025-06-12 09:17:40 -0400

    feat: google example

--- default.js
+++ default.js
@@ -1,102 +1,64 @@
-// main.js
-import { Loader } from "@googlemaps/js-api-loader";
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
-import { MarkerUtils } from "@googlemaps/markerclusterer/marker-utils.js";
-
-const MAP_ID = "DEMO_MAP_ID";
-const DEFAULT_KEY = "AIzaSyAlCk-atEVMbumOwpDHnUKSos9djbzuEpU";
-
-
-const tress = [
-  {
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-73.84421521958048, 40.723091773924274]
-    },
-    "status": "Alive",
-    "health": "Fair",
-    "spc_common": "red maple"
-  },
-  {
-    "geometry": {
-      "type": "Point",
-      "coordinates": [-73.81867945834878, 40.79411066708779]
-    },
-    "status": "Alive",
-    "health": "Fair",
-    "spc_common": "pin oak"
-  },
-];
-
-const mapOptions = {
-  center: { lat: 40.7128, lng: -73.85 },
-  zoom: 12,
-  mapId: MAP_ID,
-};
-
-new Loader(getLoaderOptions()).load().then(() => {
-  const element = document.getElementById("map");
-  const map = new google.maps.Map(element, mapOptions);
-  
-  const markers = trees.map(({ geometry }) =>
-    createMarker(map, geometry.coordinates[1], geometry.coordinates[0])
-  );
-  
-  const markerCluster = new MarkerClusterer({
-    markers,
+// This sample uses the Place Autocomplete widget to allow the user to search
+// for and select a place. The sample then displays an info window containing
+// the place ID and other information about the place that the user has
+// selected.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
   });
-  
-  markerCluster.setMap(map);
-});
-
-const getLoaderOptions = () => ({
-  apiKey: localStorage.getItem("gmaps-key") ?? DEFAULT_KEY,
-  version: "weekly",
-  libraries: ["marker"],
-});
-
-// helper function to keep maps in sync
-const sync = (...maps) => {
-  let center;
-  let zoom;
-  
-  function update(changedMap) {
-    maps.forEach((m) => {
-      if (m === changedMap) {
-        return;
-      }
-      m.setCenter(center);
-      m.setZoom(zoom);
-    });
-  }
-  
-  maps.forEach((m) => {
-    m.addListener("bounds_changed", () => {
-      const changedCenter = m.getCenter();
-      const changedZoom = m.getZoom();
-      
-      if (changedCenter !== center || changedZoom !== zoom) {
-        center = changedCenter;
-        zoom = changedZoom;
-        update(m);
-      }
-    });
+  const input = document.getElementById("pac-input");
+  // Specify just the place data fields that you need.
+  const autocomplete = new google.maps.places.Autocomplete(input, {
+    fields: ["place_id", "geometry", "formatted_address", "name"],
   });
-};
 
-// Creates a marker.
-//
-// Prefers advanced markers when they are available.
-function createMarker(map, lat, lng) {
-  if (MarkerUtils.isAdvancedMarkerAvailable(map)) {
-    return new google.maps.marker.AdvancedMarkerElement({
-      map,
-      position: { lat, lng },
+  autocomplete.bindTo("bounds", map);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  const infowindow = new google.maps.InfoWindow();
+  const infowindowContent = document.getElementById("infowindow-content");
+
+  infowindow.setContent(infowindowContent);
+
+  const marker = new google.maps.Marker({ map: map });
+
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
+  autocomplete.addListener("place_changed", () => {
+    infowindow.close();
+
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry || !place.geometry.location) {
+      return;
+    }
+
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+
+    // Set the position of the marker using the place ID and location.
+    // @ts-ignore This should be in @typings/googlemaps.
+    marker.setPlace({
+      placeId: place.place_id,
+      location: place.geometry.location,
     });
-  }
-  
-  return new google.maps.Marker({
-    position: { lat, lng },
-    map,
+    marker.setVisible(true);
+    infowindowContent.children.namedItem("place-name").textContent = place.name;
+    infowindowContent.children.namedItem("place-id").textContent =
+      place.place_id;
+    infowindowContent.children.namedItem("place-address").textContent =
+      place.formatted_address;
+    infowindow.open(map, marker);
   });
 }
+
+window.initMap = initMap;
